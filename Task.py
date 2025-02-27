748914887f44e6ddba21ec46e4cbde230f1efa0c
Saqib1155 <saqibnadeem020@gmail.com>
2025-02-27 14:02:34 +0500

    Add files via upload

---
+++ Task.py
@@ -0,0 +1,78 @@
+#Task 1
+# Create a Python  script that declares the variable in which print All information about yourself.
+
+# Personal Information
+name = "Saqib Nadeem"
+gender = "Male"
+age = "23"
+address = "Sazazar , Lahore"
+degree = "Bachelor of Information Technology"
+dob = "July 7, 2001"
+email = "saqibnadeem020.com"
+phone_number = "+923456448411"
+
+# Print all information
+print("Personal Information:")
+print(f"Name: {name}")
+print(f"Gender: {gender}")
+print(f"Age: {age}")
+print(f"Address: {address}")
+print(f"Degree: {degree}")
+print(f"Date of Birth: {dob}")
+print(f"Email: {email}")
+print(f"Phone Number: {phone_number}")
+
+# Part 2
+#  Write a Python script that demonstrates type casting while swapping two variables. And print Before and After Swapping.
+
+
+a=10
+b=15
+print("Before Swapping")
+print(a)
+print(b)
+a,b=b,print("After Swaping")
+print(a)
+print(b)
+
+# Part 3
+# Declared a Variable With different Data Types in Python and display along variable with data types.
+
+
+int=42
+float=3.75
+str="Saqib"
+list=[1,2,3,4,5]
+tuple=(10,20,30)
+dict={"name": "Alice", "age": 25}
+print(int)
+print(type(int))
+print(float)
+print(type(float))
+print(list)
+print(type(list))
+print(tuple)
+print(type(tuple))
+print(dict)
+print(type(dict))
+
+
+# Task 2
+# Part 1
+#  Write a Python  script using two variables and perform the following operations Addition, Subtraction, Multiplication, Division, Modulo
+
+# Declare two variables
+a = 15
+b = 4
+
+# Perform arithmetic operations
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+mod = a % b
+print("Addition:",add)
+print(f"Subtraction:",sub)
+print(f"Multiplication:",mul)
+print(f"Division:",div)
+print(f"Modulo:",mod)
